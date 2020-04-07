510af5ee3c8226eb7f8bebdd1a70c03485b611c3
Javier Misat <rodrigomsiat@gmail.com>
2020-04-07 15:37:57 -0500

    Menú móvil cambiado a la derecha , impplementacion patron delegation menu items

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 const burgerMenu = document.querySelector('.burguer-button');
 const menu = document.querySelector('.menu');
+const menuItems = document.querySelector('.menu ul');
 const ipad = window.matchMedia('screen and (max-width:767px)');
 
 function validation(e) {
     if (e.matches) {
         burgerMenu.addEventListener('click', menuActive);
+        menuItems.addEventListener('click', delegateMenuItems);
     } else {
         burgerMenu.removeEventListener('click', menuActive);
+        menuItems.removeEventListener('click', delegateMenuItems);
     }
 }
 
@@ -17,6 +20,13 @@ function menuActive() {
         menu.classList.add('is-active');
     }
 }
+
+function delegateMenuItems(event) {
+    if (event.target && event.target.nodeName === 'A') {
+        menu.classList.remove('is-active');
+    }
+}
+
 
 /**
  * Event listeners
